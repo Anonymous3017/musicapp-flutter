aa11ea4b3c31ef20b017d33314f09a4a7099b819
Anonymous3017 <ayushgupta9592@gmail.com>
2024-10-01 13:06:46 +0530

    Add UserLogin schema and implement login endpoint

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -6,11 +6,12 @@ from models.user import User
 from pydantic_schemas.user_create import UserCreate
 from fastapi import APIRouter
 from database import get_db
+from pydantic_schemas.user_login import UserLogin
 
 
 router = APIRouter()
 
-@router.post("/signup")
+@router.post("/signup", status_code=status.HTTP_201_CREATED)
 def signup_user(user: UserCreate, db: Session = Depends(get_db)):
     """
     Endpoint to sign up a new user.
@@ -42,3 +43,21 @@ def signup_user(user: UserCreate, db: Session = Depends(get_db)):
     db.refresh(user_db)
 
     return {"message": "User created successfully"}
+
+@router.post("/login")
+def login_user(user: UserLogin, db: Session = Depends(get_db)):
+    #check if a user with same email already exists
+    user_db = db.query(User).filter(User.email == user.email).first()
+
+    if not user_db:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Invalid credentials"
+        )
+    #password matching or not
+    if not bcrypt.checkpw(user.password.encode(), user_db.password):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Invalid credentials"
+        )
+    return user_db
