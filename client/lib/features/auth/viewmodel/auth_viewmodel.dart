524aad698b6f57d6ef96d6477de13996da2e93ba
Anonymous3017 <ayushgupta9592@gmail.com>
2024-10-07 17:04:31 +0530

    Refactor auth_viewmodel.dart and auth_local_repository.dart to initialize shared preferences and store token

--- client/lib/features/auth/viewmodel/auth_viewmodel.dart
+++ client/lib/features/auth/viewmodel/auth_viewmodel.dart
@@ -1,4 +1,5 @@
 import 'package:client/features/auth/model/user_model.dart';
+import 'package:client/features/repositories/auth_local_repository.dart';
 import 'package:client/features/repositories/auth_remote_repository.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -8,10 +9,17 @@ part 'auth_viewmodel.g.dart';
 @riverpod
 class AuthViewmodel extends _$AuthViewmodel {
   late AuthRemoteRepository _authRemoteRepository;
+  late AuthLocalRepository _authLocalRepository;
+
   @override
   AsyncValue<UserModel>? build() {
     _authRemoteRepository = ref.watch(authRemoteRepositoryProvider);
+    _authLocalRepository = ref.watch(authLocalRepositoryProvider);
     return null;
+  }
+
+  Future<void> initSharedPref() async {
+    await _authLocalRepository.init();
   }
 
   Future<void> signUpUser({
@@ -52,9 +60,14 @@ class AuthViewmodel extends _$AuthViewmodel {
           l.message,
           StackTrace.current,
         ),
-      Right(value: final r) => state = AsyncValue.data(r),
+      Right(value: final r) => _loginSuccess(r),
     };
 
     print(val);
+  }
+
+  AsyncValue<UserModel>? _loginSuccess(UserModel user) {
+    _authLocalRepository.setToken(user.token);
+    return state = AsyncValue.data(user);
   }
 }
