330387fc3c022c3f97b116665574091b259c7b17
Anonymous3017 <ayushgupta9592@gmail.com>
2024-10-07 12:20:30 +0530

    Refactor server_constants.dart to dynamically set the base URL based on the platform

--- client/lib/core/constants/server_constants.dart
+++ client/lib/core/constants/server_constants.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 class ServerConstants {
-  static const String baseUrl = 'http://10.0.2.2:8000';
+  static String baseUrl = Platform.isAndroid ? 'http://10.0.2.2:8000' : 'http://localhost:8000';
 }
