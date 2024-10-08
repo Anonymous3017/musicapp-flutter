6074f12cff4b99f79a356f0a0b5f67dec39712e8
Anonymous3017 <ayushgupta9592@gmail.com>
2024-10-08 15:27:59 +0530

    Refactor app title in main.dart to 'Music App'

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Music App',
       theme: AppTheme.dartThemeMode,
       home: const SignupPage(),
     );
