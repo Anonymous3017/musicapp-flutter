795ff4b54aa62a81d19687f5cf924a653d996904
Anonymous3017 <ayushgupta9592@gmail.com>
2024-10-08 14:17:26 +0530

    Refactor auth_remote_repository.dart to include getCurrentUserData() method for retrieving user data from the server using the auth token

--- client/lib/features/repositories/auth_remote_repository.dart
+++ client/lib/features/repositories/auth_remote_repository.dart
@@ -75,4 +75,29 @@ class AuthRemoteRepository {
       return Left(AppFailure(e.toString()));
     }
   }
+
+  Future<Either<AppFailure, UserModel>> getCurrentUserData({
+    required String token,
+  }) async {
+    try {
+      final response = await http.get(
+        Uri.parse('${ServerConstants.baseUrl}/auth/'),
+        headers: {
+          'Content-Type': 'application-json',
+          'x-auth-token': token,
+        },
+      );
+      final resBodyMap = jsonDecode(response.body) as Map<String, dynamic>;
+      if (response.statusCode != 200) {
+        return Left(AppFailure(resBodyMap['detail'] as String));
+      }
+
+      return Right(UserModel.fromMap(resBodyMap).copyWith(
+        token: token,
+      ));
+    } catch (e) {
+      print('Failed to connect to the server: $e');
+      return Left(AppFailure(e.toString()));
+    }
+  }
 }
