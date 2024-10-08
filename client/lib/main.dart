b8aab9164e07cb222085723eae09e2c2c0c5dd3e
Anonymous3017 <ayushgupta9592@gmail.com>
2024-10-08 18:08:12 +0530

    Refactor home page to use Riverpod for dependency injection and display current user

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:client/core/providers/current_user_notifier.dart';
 import 'package:client/core/theme/theme.dart';
 import 'package:client/features/auth/view/pages/login_page.dart';
 import 'package:client/features/auth/view/pages/signup_page.dart';
 import 'package:client/features/auth/viewmodel/auth_viewmodel.dart';
+import 'package:client/features/home/view/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,8 +11,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final container = ProviderContainer();
   await container.read(authViewmodelProvider.notifier).initSharedPref();
-  final userModel = await container.read(authViewmodelProvider.notifier).getData();
-  print(userModel);
+  await container.read(authViewmodelProvider.notifier).getData();
+
   runApp(
     UncontrolledProviderScope(
       container: container,
@@ -19,15 +21,16 @@ void main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentUser = ref.watch(currentUserNotifierProvider);
     return MaterialApp(
       title: 'Music App',
       theme: AppTheme.dartThemeMode,
-      home: const SignupPage(),
+      home: currentUser == null ? const SignupPage() : const HomePage(),
     );
   }
 }
