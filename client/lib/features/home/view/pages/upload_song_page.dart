d4b1e393dd9608f1232484a1f7bdb4a5cf68f78d
Anonymous3017 <ayushgupta9592@gmail.com>
2024-10-09 12:16:57 +0530

    Refactor CustomField widget to remove unnecessary code and improve code readability

--- client/lib/features/home/view/pages/upload_song_page.dart
+++ client/lib/features/home/view/pages/upload_song_page.dart
@@ -1,4 +1,5 @@
 import 'package:client/core/theme/app_pallete.dart';
+import 'package:client/core/widgets/custom_field.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -11,6 +12,16 @@ class UploadSongPage extends ConsumerStatefulWidget {
 }
 
 class _UploadSongPageState extends ConsumerState<UploadSongPage> {
+  final songNameController = TextEditingController();
+  final artistController = TextEditingController();
+
+  @override
+  void dispose() {
+    songNameController.dispose();
+    artistController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,27 +33,45 @@ class _UploadSongPageState extends ConsumerState<UploadSongPage> {
         child: Column(
           children: [
             DottedBorder(
-                color: Pallete.borderColor,
-                radius: const Radius.circular(10),
-                borderType: BorderType.RRect,
-                strokeCap: StrokeCap.round,
-                dashPattern: const [10, 4],
-                child: const SizedBox(
-                  height: 150,
-                  width: double.infinity,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.folder_open,
-                        size: 40,
-                      ),
-                      SizedBox(height: 15),
-                      Text('Select a thumbnail for your song',
-                          style: TextStyle(fontSize: 15)),
-                    ],
-                  ),
-                ))
+              color: Pallete.borderColor,
+              radius: const Radius.circular(10),
+              borderType: BorderType.RRect,
+              strokeCap: StrokeCap.round,
+              dashPattern: const [10, 4],
+              child: const SizedBox(
+                height: 150,
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.folder_open,
+                      size: 40,
+                    ),
+                    SizedBox(height: 15),
+                    Text('Select a thumbnail for your song',
+                        style: TextStyle(fontSize: 15)),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 40),
+            CustomField(
+              hintText: 'Pick Song',
+              controller: null,
+              readOnly: true,
+              onTap: () {},
+            ),
+            const SizedBox(height: 20),
+            CustomField(
+              hintText: 'Artist',
+              controller: artistController,
+            ),
+            const SizedBox(height: 20),
+            CustomField(
+              hintText: 'Song Name',
+              controller: songNameController,
+            ),
           ],
         ),
       ),
