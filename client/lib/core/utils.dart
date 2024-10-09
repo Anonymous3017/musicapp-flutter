0173ffc59a140ed04cfa58cc208bdbc010e8a49f
Anonymous3017 <ayushgupta9592@gmail.com>
2024-10-09 13:31:36 +0530

    Add file picker functionality for image and audio files

--- client/lib/core/utils.dart
+++ client/lib/core/utils.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 void showSnackBar(BuildContext context, String content, Color color) {
@@ -9,4 +12,36 @@ void showSnackBar(BuildContext context, String content, Color color) {
         backgroundColor: color,
       ),
     );
+}
+
+Future<File?> pickImage() async {
+  try {
+    final filePickerResult = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowedExtensions: ['jpg', 'jpeg', 'png'],
+    );
+
+    if (filePickerResult != null) {
+      return File(filePickerResult.files.first.xFile.path);
+    }
+    return null;
+  } catch (e) {
+    return null;
+  }
+}
+
+Future<File?> pickAudio() async {
+  try {
+    final filePickerResult = await FilePicker.platform.pickFiles(
+      type: FileType.audio,
+      allowedExtensions: ['mp3'],
+    );
+
+    if (filePickerResult != null) {
+      return File(filePickerResult.files.first.xFile.path);
+    }
+    return null;
+  } catch (e) {
+    return null;
+  }
 }
