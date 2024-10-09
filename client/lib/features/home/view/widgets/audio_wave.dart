95cb4dfcd4bd618e4b76cdb6a32f89d128b6f776
Anonymous3017 <ayushgupta9592@gmail.com>
2024-10-09 15:22:27 +0530

    Add AudioWave widget with play/pause functionality and custom styling

--- client/lib/features/home/view/widgets/audio_wave.dart
+++ client/lib/features/home/view/widgets/audio_wave.dart
@@ -1,4 +1,6 @@
 import 'package:audio_waveforms/audio_waveforms.dart';
+import 'package:client/core/theme/app_pallete.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AudioWave extends StatefulWidget {
@@ -23,6 +25,15 @@ class _AudioWaveState extends State<AudioWave> {
     playerController.preparePlayer(path: widget.path);
   }
 
+  Future<void> playAndPause() async {
+    if (!playerController.playerState.isPlaying) {
+      await playerController.startPlayer(finishMode: FinishMode.stop);
+    } else if (!playerController.playerState.isPaused) {
+      await playerController.pausePlayer();
+    }
+    setState(() {});
+  }
+
   @override
   void dispose() {
     playerController.dispose();
@@ -31,8 +42,29 @@ class _AudioWaveState extends State<AudioWave> {
 
   @override
   Widget build(BuildContext context) {
-    return AudioFileWaveforms(
-        size: const Size(double.infinity, 100),
-        playerController: playerController);
+    return Row(
+      children: [
+        IconButton(
+          onPressed: playAndPause,
+          icon: Icon(
+            playerController.playerState.isPlaying
+                ? CupertinoIcons.pause_solid
+                : CupertinoIcons.play_arrow_solid,
+          ),
+        ),
+        Expanded(
+          child: AudioFileWaveforms(
+            size: const Size(double.infinity, 100),
+            playerController: playerController,
+            playerWaveStyle: const PlayerWaveStyle(
+              fixedWaveColor: Pallete.borderColor,
+              liveWaveColor: Pallete.gradient2,
+              spacing: 6,
+              showSeekLine: false,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
