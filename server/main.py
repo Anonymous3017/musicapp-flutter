4a5767d1d5cdd6a0a14be0b7ff81b10b99be3105
Anonymous3017 <ayushgupta9592@gmail.com>
2024-10-10 15:32:26 +0530

    Add song router to main.py

--- server/main.py
+++ server/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from database import engine
 from models.base import Base
-from routes import auth
+from routes import auth, song
 
 # Initialize FastAPI app
 app = FastAPI()
 
 # Include the auth router
 app.include_router(auth.router, prefix="/auth")
+app.include_router(song.router, prefix="/song")
 
 # Create all tables
 Base.metadata.create_all(bind=engine)
