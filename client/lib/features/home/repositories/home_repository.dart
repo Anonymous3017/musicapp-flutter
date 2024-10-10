adc8e6ea17ae7edd0509af95554dd7d732a50782
Anonymous3017 <ayushgupta9592@gmail.com>
2024-10-10 16:32:45 +0530

    Refactor song upload functionality and add missing parameter
    - Refactor the uploadSong method in HomeRepository to include the missing parameter for selectedAudio.
    - Update the field value for hex_code to remove the '#' symbol.

--- client/lib/features/home/repositories/home_repository.dart
+++ client/lib/features/home/repositories/home_repository.dart
@@ -4,24 +4,25 @@ import 'package:client/core/constants/server_constants.dart';
 import 'package:http/http.dart' as http;
 
 class HomeRepository {
-  Future<void> uploadSong(File selectedImage, File selectedAudio,) async {
+  Future<void> uploadSong(
+    File selectedImage,
+    File selectedAudio,
+  ) async {
     final request = http.MultipartRequest(
         'POST', Uri.parse('${ServerConstants.baseUrl}/song/upload'));
 
     request
       ..files.addAll(
         [
-          await http.MultipartFile.fromPath(
-              'song', selectedAudio.path),
-          await http.MultipartFile.fromPath(
-              'thumbnail', selectedImage.path),
+          await http.MultipartFile.fromPath('song', selectedAudio.path),
+          await http.MultipartFile.fromPath('thumbnail', selectedImage.path),
         ],
       )
       ..fields.addAll(
         {
           'artist': 'Taylor',
           'song_name': 'Love Story',
-          'hex_code': '#FFFFFF',
+          'hex_code': 'FFFFFF',
         },
       )
       ..headers.addAll(
