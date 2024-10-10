528d972d497eb6418b13156661faefee0acd8af8
Anonymous3017 <ayushgupta9592@gmail.com>
2024-10-10 15:33:59 +0530

    Add song upload functionality using cloudinary

---
+++ server/routes/song.py
@@ -0,0 +1,43 @@
+import os
+import uuid
+import cloudinary
+import cloudinary.uploader
+from fastapi import APIRouter, Depends, File, Form, UploadFile
+from sqlalchemy.orm import Session
+from database import get_db
+from middleware.auth_middleware import auth_middleware
+from dotenv import load_dotenv
+
+
+
+router = APIRouter()
+load_dotenv()
+
+# Configuration       
+cloudinary.config( 
+    cloud_name = "ds61kwof5", 
+    api_key = "592577737874135", 
+    api_secret = os.getenv("CLOUDINARY_SECRET_KEY"),
+    secure=True
+)
+
+@router.post("/upload")
+def upload_song(song: UploadFile = File(...), 
+                thumbnail: UploadFile = File(...),
+                artist: str = Form(...), 
+                song_name: str = Form(...), 
+                hex_code: str = Form(...),
+                db: Session = Depends(get_db),
+                auth_dict = Depends(auth_middleware)):
+    
+    # Upload song to cloudinary
+
+    song_id = str(uuid.uuid4())
+    song_res = cloudinary.uploader.upload(song.file, resource_type='auto' ,folder=f"songs/{song_id}")
+    print(song_res)
+    #for thumbnauil
+    thumbnail_res = cloudinary.uploader.upload(thumbnail.file, resource_type='image' ,folder=f"songs/{song_id}")
+    print(thumbnail_res)
+
+    #store this data in db
+    return 'ok'
