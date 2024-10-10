903120fadba742e928431fb5297882436e5c0b2e
Anonymous3017 <ayushgupta9592@gmail.com>
2024-10-10 17:23:05 +0530

    Refactor song upload functionality and add error handling

--- client/lib/features/home/repositories/home_repository.dart
+++ client/lib/features/home/repositories/home_repository.dart
@@ -1,38 +1,48 @@
 import 'dart:io';
 
 import 'package:client/core/constants/server_constants.dart';
+import 'package:client/core/failure/failure.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:http/http.dart' as http;
 
 class HomeRepository {
-  Future<void> uploadSong(
+  Future<Either<AppFailure, String>> uploadSong(
     File selectedImage,
     File selectedAudio,
   ) async {
-    final request = http.MultipartRequest(
-        'POST', Uri.parse('${ServerConstants.baseUrl}/song/upload'));
+    try {
+      final request = http.MultipartRequest(
+          'POST', Uri.parse('${ServerConstants.baseUrl}/song/upload'));
 
-    request
-      ..files.addAll(
-        [
-          await http.MultipartFile.fromPath('song', selectedAudio.path),
-          await http.MultipartFile.fromPath('thumbnail', selectedImage.path),
-        ],
-      )
-      ..fields.addAll(
-        {
-          'artist': 'Taylor',
-          'song_name': 'Love Story',
-          'hex_code': 'FFFFFF',
-        },
-      )
-      ..headers.addAll(
-        {
-          'x-auth-token':
-              'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjliMDY5ZmY0LWI0ZmUtNDc4ZS1iOGU5LWJkNzUzNzQyMWNmZiJ9.Q2vts1hcmaTORCh9085YXf6HGozCwM5GJ77zrdXigUI'
-        },
-      );
+      request
+        ..files.addAll(
+          [
+            await http.MultipartFile.fromPath('song', selectedAudio.path),
+            await http.MultipartFile.fromPath('thumbnail', selectedImage.path),
+          ],
+        )
+        ..fields.addAll(
+          {
+            'artist': 'Taylor',
+            'song_name': 'Love Story',
+            'hex_code': 'FFFFFF',
+          },
+        )
+        ..headers.addAll(
+          {
+            'x-auth-token':
+                'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjliMDY5ZmY0LWI0ZmUtNDc4ZS1iOGU5LWJkNzUzNzQyMWNmZiJ9.Q2vts1hcmaTORCh9085YXf6HGozCwM5GJ77zrdXigUI'
+          },
+        );
 
-    final res = await request.send();
-    print(res);
+      final res = await request.send();
+
+      if (res.statusCode != 201) {
+        return Left(AppFailure(await res.stream.bytesToString()));
+      }
+      return Right(await res.stream.bytesToString());
+    } catch (e) {
+      return Left(AppFailure(e.toString()));
+    }
   }
 }
