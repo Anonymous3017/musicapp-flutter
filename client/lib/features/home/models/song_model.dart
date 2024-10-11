4fbe9066fe45fc2665525f1b63c8cd64a8b0a8be
Anonymous3017 <ayushgupta9592@gmail.com>
2024-10-11 13:00:06 +0530

    Refactor SongModel class to fix formatting issues and add missing commas in the fromMap method

--- client/lib/features/home/models/song_model.dart
+++ client/lib/features/home/models/song_model.dart
@@ -18,7 +18,6 @@ class SongModel {
     required this.hex_code,
   });
 
-
   SongModel copyWith({
     String? id,
     String? song_name,
@@ -50,18 +49,19 @@ class SongModel {
 
   factory SongModel.fromMap(Map<String, dynamic> map) {
     return SongModel(
-      id: map['id'] ?? ''
-      song_name: map['song_name'] ?? ''
-      artist: map['artist'] ?? ''
-      thumbnail_url: map['thumbnail_url'] ?? ''
-      song_url: map['song_url'] ?? ''
-      hex_code: map['hex_code'] ?? ''
+      id: map['id'] ?? '',
+      song_name: map['song_name'] ?? '',
+      artist: map['artist'] ?? '',
+      thumbnail_url: map['thumbnail_url'] ?? '',
+      song_url: map['song_url'] ?? '',
+      hex_code: map['hex_code'] ?? '',
     );
   }
 
   String toJson() => json.encode(toMap());
 
-  factory SongModel.fromJson(String source) => SongModel.fromMap(json.decode(source) as Map<String, dynamic>);
+  factory SongModel.fromJson(String source) =>
+      SongModel.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
   String toString() {
@@ -71,23 +71,22 @@ class SongModel {
   @override
   bool operator ==(covariant SongModel other) {
     if (identical(this, other)) return true;
-  
-    return 
-      other.id == id &&
-      other.song_name == song_name &&
-      other.artist == artist &&
-      other.thumbnail_url == thumbnail_url &&
-      other.song_url == song_url &&
-      other.hex_code == hex_code;
+
+    return other.id == id &&
+        other.song_name == song_name &&
+        other.artist == artist &&
+        other.thumbnail_url == thumbnail_url &&
+        other.song_url == song_url &&
+        other.hex_code == hex_code;
   }
 
   @override
   int get hashCode {
     return id.hashCode ^
-      song_name.hashCode ^
-      artist.hashCode ^
-      thumbnail_url.hashCode ^
-      song_url.hashCode ^
-      hex_code.hashCode;
+        song_name.hashCode ^
+        artist.hashCode ^
+        thumbnail_url.hashCode ^
+        song_url.hashCode ^
+        hex_code.hashCode;
   }
 }
